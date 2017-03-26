7d1f2b5965aea7ac69245fb8a49cf49163605fb1
kumar arunav rout <arunav.rout93@gmail.com>
2017-03-27 02:57:41 +0530

    salary payment

---
+++ LCLCDKPaymentService/Models/SalaryPayment.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LCLCDKPaymentService.Models
+{
+    public class SalaryPayment
+    {
+        string SYSTEM_DATA { get; set; }
+        string AFTALE{ get; set; }
+        string BRGNR { get; set; }
+        string SPROG { get; set; }
+        string LANDEKODE { get; set; }
+        string INTEKSTBRUGER { get; set; }
+        string LDKD_BRUGER { get; set; }
+        string SPKD_BRUGER { get; set; }
+        string TZID_BRUGER { get; set; }
+        string KT_DROPDOWN_SPECIEL { get; set; }
+        string CHKDDSTD3 { get; set; }
+        string TXOAFSVLG { get; set; }
+        string FUNDETDDROPDOWNOPL { get; set; }
+        string CREDITORNAMEID { get; set; }
+        string CREDITORNAME { get; set; }
+        string CREDITOR_ID { get; set; }
+        string TEMPLATE_ID { get; set; }
+        string CREDITOR_STATUS { get; set; }
+        string TEMPLATE_STATUS { get; set; }
+        string IB_AKTUEL_HEND_TS { get; set; }
+        string MODTAGERNR { get; set; }
+        string IB_FRA_KONTO_INTERN { get; set; }
+        string IB_AFSENDER_REF { get; set; }
+        string IB_TIL_KONTO { get; set; }
+        string IB_MODTAGER_REF { get; set; }
+        string IB_DEBET_BELQB { get; set; }
+        string IB_VALUTAKODE { get; set; }
+        string IB_EKSP_DATO { get; set; }
+        string AntalDec { get; set; }
+        string IP_DATA_GR { get; set; }
+        string LSID { get; set; }
+        string BETALINGSKATEGORI { get; set; }
+        string CheckFortsAt { get; set; }
+        string CheckGodkend { get; set; }
+        string CheckLuk { get; set; }
+        string checkSaveFol { get; set; }
+        string checkNewPay { get; set; }
+        string checkAuthPay { get; set; }
+        string checkUnauPay { get; set; }
+        string BTTYtekst { get; set; }
+        string checkGemMappe { get; set; }
+        string checkGemBtty { get; set; }
+        string ChkModtGem { get; set; }
+        string FoldetUd { get; set; }
+    }
+}
